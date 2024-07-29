fc7e61c203b6240a7cf0b4494c69513b1e059ccb
Mihai Gaitos (hhw) <hawk@hawk.ro>
2024-07-29 19:23:15 +0200

    fixed bld.bat

--- bld.bat
+++ bld.bat
@@ -6,7 +6,7 @@ mkdir output 2>NUL:
 rem call "c:\Program Files\PellesC\Bin\povars32.bat"
 del "%PROGNAME%.exe"
 echo compiling %PROGNAME%
-pocc.exe -std:C11 -I..\mylibs -Tx86-coff -Ot -Ob1 -fp:precise -W1 -Gz -Ze -Go -DUNICODE -D_UNICODE -DDEBUG limg2.c -Fo"output\limg2.obj"
+pocc.exe -std:C11 -Tx86-coff -Ot -Ob1 -fp:precise -W1 -Gz -Ze -Go "%COMPOPT%" limg2.c -Fo"output\limg2.obj"
 pocc.exe -std:C11 -Tx86-coff -Ot -Ob1 -fp:precise -W1 -Gz -Ze -Go "%COMPOPT%" "%PROGNAME%.c" -Fo"output\%PROGNAME%.obj"
 porc.exe "%PROGNAME%.rc" -Fo"output\%PROGNAME%.res"
 echo linking
